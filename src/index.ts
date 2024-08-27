84f1ba9a17fffbba9c8bd185032285abe466787b
Brett <27568879+BrettCleary@users.noreply.github.com>
2024-08-27 00:08:33 -0700

    [Feat] add setStyle option (#1)
    * add setStyle
    
    * add missing pkg

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,30 @@
-export function requestFullscreen(elementId?: string){
-    window.parent.postMessage({
-        method: 'requestFullscreen'
-    }, '*')
+export function requestFullscreen({
+  elementId,
+  setStyle
+}: {
+  elementId?: string
+  setStyle?: boolean
+}) {
+  window.parent.postMessage(
+    {
+      method: 'requestFullscreen'
+    },
+    '*'
+  )
 
-    if (elementId){
-        document.getElementById(elementId)?.requestFullscreen()
+  if (elementId) {
+    const element = document.getElementById(elementId)
+    element?.requestFullscreen()
+    if (setStyle && element) {
+      element.style.setProperty('position', 'fixed')
+      element.style.setProperty('top', '0px')
+      element.style.setProperty('right', '0px')
+      element.style.setProperty('bottom', '0px')
+      element.style.setProperty('left', '0px')
+      element.style.setProperty('width', '100%')
+      element.style.setProperty('height', '100%')
+      element.style.setProperty('border', 'none')
+      element.style.setProperty('overflow', 'hidden')
     }
+  }
 }
